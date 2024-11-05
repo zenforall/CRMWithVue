dee5f704c58f1ce221aaf4d2725deadb67ba15e4
Stefano Gallina <59648879+zenforall@users.noreply.github.com>
2024-11-05 14:59:44 +0100

    Update LoginView.vue
    Aggiunti componenti alla pagina

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,6 +7,11 @@ const password = ref("");
 </script>
 
 <template>
+    <v-container fluid fill-height>
+        <v-row align-center justify-center>
+          <v-col xs12 sm8 md4>
 
-
+          </v-col>
+        </v-row>
+    <v-container>
 </template>
