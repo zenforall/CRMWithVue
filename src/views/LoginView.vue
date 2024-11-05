8b2ad8407df1741458be2ec71661d3c029b69aeb
Stefano Gallina <59648879+zenforall@users.noreply.github.com>
2024-11-05 15:15:42 +0100

    Update LoginView.vue
    Aggiunto commento

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+// https://codesandbox.io/p/sandbox/vuetify-login-base-forked-kzy5pr?file=%2Fsrc%2Fcomponents%2FLogin.vue%3A7%2C1 : link per prendere ispirazione
+  
 import { ref } from "vue";
 
 const username = ref("");
