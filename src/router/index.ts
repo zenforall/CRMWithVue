a806a52c7221a0cf7c57d2a8316717a704f5b598
Stefano Gallina <stefano.gallina@venistar.retex.com>
2024-12-09 15:28:09 +0100

    Tentativi di modificare i file di configurazione per eseguire correttamente il deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView_2.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/LoginView_2.vue'),
+      component: LoginView,
     },
     {
       path: '/about',
@@ -15,7 +15,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: LoginView,
     },
     {
       path: '/login',
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: '/CRMWithVue/'
+    publicPath: process.env.NODE_ENV === "production" ? "/CRMWithVue/" : "/",
 }
