864b849d4f6855a139ffedacef51187be35c4178
Stefano Gallina <stefano.gallina@venistar.retex.com>
2025-01-23 15:59:35 +0100

    Diviso lo script per eseguire il build ed il deploy dell'applicazione

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,1 @@
 npm run build
-cd dist
-
-echo "Sto per fare il deploy"
-
-git init
-git add -A
-git commit -m 'New Deployment'
-git push -f git@github.com:zenforall/CRMWithVue.git master:gh-pages
-
-cd ..
---
+++ deploy_in_prod.bat
@@ -0,0 +1,10 @@
+cd dist
+
+echo "Sto per fare il deploy"
+
+git init
+git add -A
+git commit -m 'New Deployment'
+git push -f git@github.com:zenforall/CRMWithVue.git master:gh-pages
+
+cd ..
