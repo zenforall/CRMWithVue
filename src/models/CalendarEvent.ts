2d50917684d6bd1ad9246352192675578b78ce6d
Stefano Gallina <59648879+zenforall@users.noreply.github.com>
2025-03-20 17:06:12 +0100

    Creazione delle strutture per la gestione degli eventi del calendario

--- src/models/CalendarEvent.ts
+++ src/models/CalendarEvent.ts
@@ -2,6 +2,10 @@ export interface CalendarEvent {
   id:string,
   name:string,
   description:string,
+  contact:Contact,
+  day:number,
+  month:number,
+  year:number,
   dateStart:Date,
   dateEnd:Date,
 }
