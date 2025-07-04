599abb6645c3921df69e0963a3073a6aa38ffa77
Stefano Gallina <59648879+zenforall@users.noreply.github.com>
2025-07-04 16:10:56 +0200

    Modificata pagina del Calendar

--- src/views/activites/CalendarView.vue
+++ src/views/activites/CalendarView.vue
@@ -4,6 +4,11 @@ import Calendar from '@/components/Calendar.vue';
 import type { BreadCrumbItem } from '@/models/BreadCrumbItem';
 import { onMounted, onUnmounted } from 'vue';
 
+
+ const items = [] as string[];
+    items.push("Activities");
+    items.push("Calendar");
+
 const emit =defineEmits<{
       (event: "breadCrumbHandler", message: BreadCrumbItem[]): void;
     }>();
@@ -34,10 +39,38 @@ const emit =defineEmits<{
 </script>
 
 <template>
+
+<v-card style="margin-right: 5px;margin-bottom: 10px;margin-top: 10px;background-color: #f9f9f9;" elevation="0">
+  <v-row style="display: flex;align-items: center;background-color: white;border: solid black 0px;">
+    <v-col style="margin-top: 10px;margin-left: 20px;padding: 10px;">
+      <v-breadcrumbs :items="items" color="secondary">
+        <template v-slot:divider>
+          /
+        </template>
+       </v-breadcrumbs>
+    </v-col>
+  </v-row>
+  <v-row style="display: flex;align-items: center;background-color: white;border: 0px solid black;">
+    <v-col style="margin-top: 0px;margin-left: 20px;padding: 10px;">
+       <v-label class="text-secondary" style="font-weight: bold;font-size: x-large;opacity: 0.87;">Calendar</v-label>
+    </v-col>
+    <v-col style="display: flex;justify-content: end;margin-right: 5px;margin-top: 0px;margin-bottom: 10px;padding: 10px;">
+       <v-btn  style="margin-left: 5px;background-color:#03a840;color: #fff;">Create Event</v-btn>
+    </v-col>
+  </v-row>
+</v-card>
+
+
    <Calendar></Calendar>
 </template>
 
-<style>
+<style scoped>
+:deep(.v-breadcrumbs--density-default) {
+  padding: 0px;
+}
 
+:deep(.v-breadcrumbs-item ) {
+  padding:0px;
+}
 
 </style>
